78ff5c53bac9b19c2619266a4655263016a987e1
asifs-cse <asifs.cse@gmail.com>
2021-05-24 23:42:30 +0530

    first

---
+++ Python/Solving/List/Find the Runner-up score.py
@@ -0,0 +1,5 @@
+# Find the Runner-Up Score! in Python - Hacker Rank Solution
+if __name__ == '__main__':
+    n = int(raw_input())
+    arr = map(int, raw_input().split())
+    print(sorted(list(set(arr)))[-2])
